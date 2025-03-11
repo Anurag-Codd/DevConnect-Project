b1b1feb8c8c2ebe0e9054b62915b12839a71484d
Anurag-Codd <anuragprateek124@gmail.com>
2025-03-12 02:33:10 +0530

    hide .env keys

--- frontend/src/lib/firebase.js
+++ frontend/src/lib/firebase.js
@@ -1,14 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-const firebaseConfig = {
-    apiKey: "AIzaSyAgMVdpYjDkBgb1RGx55OvBYo4iw4KhlZk",
-    authDomain: "dev-cnt.firebaseapp.com",
-    projectId: "dev-cnt",
-    storageBucket: "dev-cnt.firebasestorage.app",
-    messagingSenderId: "712347428860",
-    appId: "1:712347428860:web:2d414ce74a3213d719f479"
-  };
+const firebaseConfig = JSON.parse(import.meta.env.FIREBASE_CONFIG);
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
